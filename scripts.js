983e5b8bc9cb3031c4125677ea21eeb471fee810
Patrick Hogan <phogan110@live.com>
2018-10-29 14:22:47 -0600

    created function `whoIAm`

--- scripts.js
+++ scripts.js
@@ -12,3 +12,9 @@ let getFirstName = (fullName) => {
 };
 
 let fName = (fullName) => console.log(fullName.split(' ')[0]);
+
+let numVal = (x, y) => ({exponent: x^y, addSum: x + y});
+
+console.log(`the exponent of 4 and 9 is ${numVal(4, 9).exponent}`)
+
+console.log(`the sum of 4 and 9 is ${numVal(4, 9).addSum}`)
