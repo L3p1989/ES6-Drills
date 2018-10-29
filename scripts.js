c222a80d3d270a0c146c032275be94c273804fe0
Patrick Hogan <phogan110@live.com>
2018-10-29 13:29:40 -0600

    reformatted favMovie into arrow function

--- scripts.js
+++ scripts.js
@@ -1,11 +1,4 @@
-function favMovie(movie, name) {
-    if (typeof movie === 'undefined') {
-        movie = "The Room";
-    } if (typeof name === 'undefined') {
-        name = "world"
-    }
-    console.log(`my name is ${name} my favorite movie is ${movie}`);
-};
+let favMovie = (movie = "The Room", name = "world") => console.log(`my name is ${name} my favorite movie is ${movie}`);
 
 favMovie();
 favMovie("world");
