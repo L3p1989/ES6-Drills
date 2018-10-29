5a9358948b4b6f90ed8f161d3505a3c519b6014d
Patrick Hogan <phogan110@live.com>
2018-10-29 11:15:32 -0600

    created favMovie par `name`; gave default of 'world'; called favMovie twice

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
-function favMovie(movie) {
+function favMovie(movie, name) {
     if (typeof movie === 'undefined') {
         movie = "The Room";
+    } if (typeof name === 'undefined') {
+        name = "world"
     }
-    console.log(`my favorite movie is ${movie}`);
+    console.log(`my name is ${name} my favorite movie is ${movie}`);
 };
 
 favMovie();
 favMovie("world");
+
+favMovie("The Golden Compass", "Patrick");
+favMovie();
